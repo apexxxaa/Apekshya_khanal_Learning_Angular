a0f911d2ba3be94b235c4de271fd49c752359a14
matt <mhaug@stclaircollege.ca>
2024-10-10 15:37:01 -0400

    Fixed Assignment 3

--- src/app/car-list-item/car-list-item.component.ts
+++ src/app/car-list-item/car-list-item.component.ts
@@ -1,17 +1,16 @@
 import {Component, Input} from '@angular/core';
+import {NgIf} from "@angular/common";
+import {Cars} from "../models/cars";
 
 @Component({
   selector: 'app-car-list-item',
   standalone: true,
-  imports: [],
+  imports: [
+    NgIf
+  ],
   templateUrl: './car-list-item.component.html',
   styleUrl: './car-list-item.component.css'
 })
 export class CarListItemComponent {
-  @Input() car?: {
-    make: string;
-    model: string;
-    year: number;
-    color: string;
-  } ;
+  @Input() car?: Cars;
 }
