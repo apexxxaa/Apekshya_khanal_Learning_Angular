5cba92585fd6c5702a30c1b984abb0379f749dd9
Apekshya <W0842585@myscc.ca>
2024-10-16 22:59:36 -0400

    "removed the array out of ContentList component and put into its own file created service  "

--- src/app/car-list/car-list.component.ts
+++ src/app/car-list/car-list.component.ts
@@ -13,31 +13,5 @@ import {NgForOf} from "@angular/common";
   styleUrl: './car-list.component.css'
 })
 export class CarListComponent {
-  cars = [
-    {
-      make: 'Tesla',
-      model: 'Model S',
-      year: 2022,
-      color: 'Red'
-    },
-    {
-      make: 'Ford',
-      model: 'Mustang',
-      year: 2021,
-      color: 'blue'
 
-    },
-    {
-      make: 'Chevrolet',
-      model: 'camaro',
-      year: 2020,
-      color: 'Yellow'
-    },
-    {
-      make: 'Honda',
-      model: 'civic',
-      year: 2019,
-      color: 'black'
-    }
-  ];
 }
